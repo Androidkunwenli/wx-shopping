07fbcf5da0ad003e1a30c7c39244cab0348de7d9
zhangdong <1940978083@qq.com>
2019-08-16 09:44:12 +0800

    11

--- pages/mycoupons/index.js
+++ pages/mycoupons/index.js
@@ -7,29 +7,6 @@ Page({
   },
   onLoad: function () {
   },
-  onShow : function () {
-    this.getMyCoupons();
-  },
-  getMyCoupons: function () {
-    var that = this;
-    wx.request({
-      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/discounts/my',
-      data: {
-        token: app.globalData.token,
-        status: 0
-      },
-      success: function (res) {
-        if (res.data.code == 0) {
-          var coupons = res.data.data;
-          if (coupons.length > 0) {
-            that.setData({
-              coupons: coupons
-            });
-          }
-        }
-      }
-    })
-  },
   goBuy:function(){
     wx.reLaunch({
       url: '/pages/index/index'
